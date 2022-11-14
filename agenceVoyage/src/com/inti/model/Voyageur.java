a72bbee128c8fb3a8a85c342d7baa16b613cfc78
aspalrobin <robin.colliaux@gmail.com>
2022-11-14 16:28:04 +0100

    voyageur, reservation

--- agenceVoyage/src/com/inti/model/Voyageur.java
+++ agenceVoyage/src/com/inti/model/Voyageur.java
@@ -1,5 +1,20 @@
 package com.inti.model;
 
-public class Voyageur {
+import java.time.LocalDate;
 
+public class Voyageur {
+	Long idVoyageur;
+	String nom;
+	String prenom;
+	int age;
+
+	
+	public Voyageur(Long idVoyageur, String nom, String prenom,int age) {
+		
+		this.idVoyageur = idVoyageur;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.age = age;
+		
+}
 }
